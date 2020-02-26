fd5ab78081c2f226ad95b29544d6d82518cce393
Jan-Willem van Heeswijck <janwillem.vh@gmail.com>
2020-02-26 10:22:37 +0100

    sidebar toggle implemented

--- app/static/js/force-graph.js
+++ app/static/js/force-graph.js
@@ -1,2 +1,11 @@
 var domain = document.domain;
 
+margin = ;
+var width = 0'
+var height = 0;
+
+
+d3.select('#canvasdiv')
+.append('svg')
+
+
--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,6 +1,20 @@
-// Establish Connection
-var socket = io.connect(base_url);
-socket.on('connect', function() {
-    console.log('Websocket connected!');
+// // Establish Connection
+// var socket = io.connect(base_url);
+// socket.on('connect', function() {
+//     console.log('Websocket connected!');
+// });
+
+$(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+        let value = $('#sidebarCollapse span').text();
+        if (value == "<"){
+            $('#sidebarCollapse span').text('>')
+        } else {
+            $('#sidebarCollapse span').text('<')
+        }
+    });
 });
+
+
 
