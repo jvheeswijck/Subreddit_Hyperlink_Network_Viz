43b55adda7830ceee31e94920d269d1df6eef52a
Jan-Willem van Heeswijck <janwillem.vh@gmail.com>
2020-02-26 22:40:14 +0100

    hover-over adjustments

--- app/static/js/graph.js
+++ app/static/js/graph.js
@@ -8,6 +8,8 @@ var margin = { top: 0, right: 0, bottom: 0, left: 0 },
 // var max_zoom = 7;
 // var zoom = d3.zoom().scaleExtent([min_zoom,max_zoom])
 
+var nodeScale = d3.scaleSqrt();
+
 
 var svg = d3.select('#svg-div')
     .append('svg')
@@ -59,20 +61,33 @@ loaded_data.then(function (data) {
         // .style("fill", "#69b3a2")
         .style("fill", "rgb(172, 220, 114)")
         .style("opacity", 0.8)
-        .on('mouseover', mouseOverFunction)
+        .on('mouseover', nodeOverFunction)
         .on('mousemove', () => tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px"))
-        .on('mouseout', () => tooltip.style("visibility", "hidden"))
+        .on('mouseout', nodedOutFunction)
 
 })
 
 
-const mouseOverFunction = d => {
+function nodeOverFunction(d){
     tooltip.style("visibility", "visible")
         .html(() => {
             const content = `<strong>Subreddit:</strong> <span>${d.sub}</span>`
             return content;
         })
+    d3.select(this)
+    .transition()
+    .duration(100)
+    .attr('r', '12px')
+    
 };
+
+function nodedOutFunction(){
+    d3.select(this)
+    .transition()
+    .duration(100)
+    .attr('r', '8px')
+    tooltip.style("visibility", "hidden")
+}
 
 tooltip = d3.select("body").append("div")
   .attr("class", "svg-tooltip")
