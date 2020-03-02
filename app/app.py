af22059801be8fdcb70cab2ccfe8a4bae175e112
Jan-Willem van Heeswijck <janwillem.vh@gmail.com>
2020-03-02 01:25:17 +0100

    data retrieval changes and filtering code

--- app/app.py
+++ app/app.py
@@ -15,6 +15,10 @@ app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
 socketio = SocketIO(app)
 
+def split_sentiment(df):
+    mask = df.sentiment == 1
+    return [df[mask], df[~mask]]
+
 df_points = pd.read_csv('../data/projections/perp_10_2500subs.csv')
 df_title = pd.read_csv('../data/soc-redditHyperlinks-title.tsv', sep='\t')
 df_body = pd.read_csv('../data/soc-redditHyperlinks-body.tsv', sep='\t')
@@ -23,33 +27,33 @@ df_all = pd.concat([df_title, df_body])
 all_subs = set()
 all_subs.update(df_points['sub'])
 
-df_all = df_all[df_all.SOURCE_SUBREDDIT.map(lambda x: x in all_subs) | df_all.TARGET_SUBREDDIT.map(lambda x: x in all_subs)]
+df_all = df_all[df_all.SOURCE_SUBREDDIT.map(lambda x: x in all_subs) & df_all.TARGET_SUBREDDIT.map(lambda x: x in all_subs)]
 df_all.TIMESTAMP = pd.to_datetime(df_all.TIMESTAMP)
+df_all.columns = "source target post_id timestamp sentiment props".split()
 
-df_source = df_all.set_index('SOURCE_SUBREDDIT')
-df_target = df_all.set_index('TARGET_SUBREDDIT')
-
+df_source = df_all.set_index('source')
+df_target = df_all.set_index('target')
 df_current = df_all
 
 # Shift to 0 origin
 x_shift = df_points.x.min()
 y_shift = df_points.y.min()
-
 df_points.x = df_points.x + (-x_shift)
 df_points.y = df_points.y +(-y_shift)
 
-bins = pd.cut(df_all.TIMESTAMP, 50)
+# Scent Calculations
+bins = pd.cut(df_all.timestamp, 50)
 df_all['bin'] = bins
-pos_rows = df_all[df_all.LINK_SENTIMENT == 1]
-neg_rows = df_all[df_all.LINK_SENTIMENT == -1]
+pos_rows,neg_rows = split_sentiment(df_all)
 
+links = df_all.groupby(['source', 'target', 'sentiment'])['post_id'].count().reset_index()
+links.columns = 'source target sentiment n'.split()
+links = links.sort_values(by='n', ascending=False)
 
 
 with open('../data/tags/tag_hierarchical.json', 'r') as f:
     tag_graph = json.load(f)
     
-
-# tag_filter
 
 def prepare_csv(df):
     csv_obj = StringIO()
@@ -60,15 +64,15 @@ def prepare_csv(df):
 
 @app.route("/", methods=['POST', 'GET'])
 def home():
-    first_link = df_all.TIMESTAMP.min().ctime()
-    last_link = df_all.TIMESTAMP.max().ctime()
+    first_link = df_all.timestamp.min().ctime()
+    last_link = df_all.timestamp.max().ctime()
     return render_template('index.html', first_link=first_link, last_link=last_link)
 
 @app.route('/data')
 def serve_data():
     if request.args.get('g') == 'volume_hist':
-        df_vol = pd.DataFrame(pos_rows.groupby('bin')['POST_ID'].count().values, columns=['positive'])
-        df_vol['negative'] = neg_rows.groupby('bin')['POST_ID'].count().values
+        df_vol = pd.DataFrame(pos_rows.groupby('bin')['post_id'].count().values, columns=['positive'])
+        df_vol['negative'] = neg_rows.groupby('bin')['post_id'].count().values
         return prepare_csv(df_vol)
 
     if request.args.get('g') == 'sub_points':
@@ -82,12 +86,39 @@ def on_date_update(dates):
     df_current = df_all
     emit('update_graph', {'one':'msg'})
     
+@socketio.on('label_change')
+def on_label_update(dates):
+    df_current = df_all
+    emit('update_graph', {'one':'msg'})
 
 @app.route("/jsondata",methods=["GET","POST"])
 def returnjson():
     return send_file("../data/sourcetarget.json")
 
+@app.route("/nodes",methods=["GET","POST"])
+def get_nodes():
+    return prepare_csv(df_points)
 
+@app.route("/links",methods=["GET","POST"])
+def get_links():
+    return prepare_csv(links.iloc[:2000])
+
+
+def filter_data():
+    # Filter by Date
+    # df_current
+    # df_current = 
+    # Filter by Source
+    # df_current =
+    # Filter by Destination
+    # df_current
+    pass
+
+def create_links():
+    pass
+
+def create_node_sumamry():
+    pass
 
 if __name__ == "__main__":
     socketio.run(app, host='0.0.0.0', use_reloader = True)
--- app/static/js/mygraph2.js
+++ app/static/js/mygraph2.js
@@ -1,12 +1,19 @@
 var base_url = `http://${document.domain}:${location.port}`;
 
+// Default Settings
+var default_circle_min_radius = 4;
+var default_circle_max_radius = 12;
+var default_opacity = 0.8;
+
+// Margins
 var margin = { top: 0, right: 0, bottom: 0, left: 0 },
     width = $("#svg-div").parent().width() - margin.left - margin.right,
     height = $("#svg-div").parent().height() - margin.top - margin.bottom;
 
-
-var nodeScale = d3.scaleSqrt();
-
+// Scaling
+var xScale = null;
+var yScale = null;
+var nodeScale = d3.scaleSqrt().range([default_circle_min_radius, default_circle_max_radius]);
 
 var svg = d3.select('#svg-div')
     .append('svg')
@@ -14,7 +21,7 @@ var svg = d3.select('#svg-div')
     .attr('preserveAspectRatio', 'xMinYMin meet')
     .classed('svg-content', true);
 
-//  FIGURE OUT THIS VIEWBOX STUFF
+// Pan and Zoom
 var transform = d3.zoomIdentity.translate(100, 50).scale(0.8)
 var zoom = d3.zoom()
     .scaleExtent([0.2, 10])
@@ -26,96 +33,153 @@ var zoom = d3.zoom()
         svg.selectAll('line')
             .attr('transform', d3.event.transform);
         tooltip.attr('transform', d3.event.transform);
-        // style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");
     });
-
-
-
 svg.call(zoom);
 
-var xScale = null;
-var yScale = null;
 
+// Force Graph
 var simulation = d3.forceSimulation()
-    // .force("charge", null)//.strength(-200))
-    .force("link", d3.forceLink().id(function (d) { return d.sub; }))//.distance(40))
-    // .force("x", d3.forceX(width / 2))
-    // .force("y", d3.forceY(height / 2))
+    // .force("link", d3.forceLink().id((d) => d.sub))//.distance(40))
     .force('collision', d3.forceCollide().radius(4))
     .on("tick", ticked);
 
-// simulation.stop();
+var links = svg
+    .append('g')
+    .attr('id', 'links')
+    .selectAll(".link");
 
-
-var link = svg.selectAll(".link"),
-    node = svg.selectAll(".node");
+var nodes = svg
+    .append('g')
+    .attr('id', 'nodes')
+    .selectAll(".node");
 
 var adjlist = [];
 
-jsonfile = d3.json("/jsondata").then(function (data) {
+let link_data = null;
+let node_data = null;
+var nodeById = d3.map();
 
+// Load and Draw Data
+d3.csv("/nodes").then(function (data_node) {
+    d3.csv('/links').then(function (data_link) {
+        
+        link_data = data_link;
+        node_data = data_node;
 
-    console.log(data)
+        node_data.forEach(function (node) {
+            nodeById.set(node.sub, node);
+        });
 
-    xScale = d3.scaleLinear()
-        .domain(d3.extent(data.nodes, (d) => d.x))
-        .range([0, width])
+        link_data.forEach(function (link) {
+            link.source = nodeById.get(link.source);
+            link.target = nodeById.get(link.target);
+        });
 
+        xScale = d3.scaleLinear()
+            .domain(d3.extent(node_data, (d) => d.x))
+            .range([0, width])
 
-    yScale = d3.scaleLinear()
-        .domain(d3.extent(data.nodes, (d) => d.y))
-        .range([0, height])
+        yScale = d3.scaleLinear()
+            .domain(d3.extent(node_data, (d) => d.y))
+            .range([0, height])
 
-    simulation.nodes(data.nodes);
-    simulation.force("link").links(data.links);
-
-    node = node
-        .data(data.nodes)
-        .enter().append("circle")
-        .attr("cx", function (d) { return xScale(d.x); })
-        .attr("cy", function (d) { return yScale(d.y); })
-        .attr("class", "node")
-        .attr("r", 6)
-        .style("fill", "rgb(172, 220, 114)")
-        .style("opacity", 0.8)
-        .on('mouseover', nodeOverFunction)
-        .on('mousemove', () => tooltip.style("top", (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px"))
-        .on('mouseout', nodedOutFunction);
+        nodes = nodes
+            .data(node_data)
+            .enter()
+            .append("circle")
+            .attr("cx", (d) => xScale(d.x))
+            .attr("cy", (d) => yScale(d.y))
+            .attr("class", "node")
+            .attr("r", 4)
+            .style("fill", "rgb(172, 220, 114)")
+            .style("opacity", 0.8)
+            .on('mouseover', nodeOverFunction)
+            .on('mousemove', () => tooltip.style("top", (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px"))
+            .on('mouseout', nodedOutFunction);
         // .call(drag(simulation));
+
+        links = links
+            .data(link_data)
+            .enter()
+            .append("line")
+            .attr("stroke", "#aaa")
+            .attr("stroke-width", "1px")
+            .attr("x1", (d) => xScale(d.source.x))
+            .attr("y1", (d) => yScale(d.source.y))
+            .attr("x2", (d) => xScale(d.target.x))
+            .attr("y2", (d) => yScale(d.target.y))
+
+        link_data.forEach(function (d) {
+            adjlist[d.source.index + "-" + d.target.index] = true;
+            adjlist[d.target.index + "-" + d.source.index] = true;
+        });
 
         svg.call(zoom.transform, transform);
 
-
-    //console.log(node)
-    link = link
-        .data(data.links)
-        .enter()
-        .append("line")
-        .attr("stroke", "#aaa")
-        .attr("stroke-width", "1px")
-        .attr("x1", (d) => xScale(d.source.x))
-        .attr("y1", (d) => yScale(d.source.y))
-        .attr("x2", (d) => xScale(d.target.x))
-        .attr("y2", (d) => yScale(d.target.y))
-
-
-    data.links.forEach(function (d) {
-        adjlist[d.source.index + "-" + d.target.index] = true;
-        adjlist[d.target.index + "-" + d.source.index] = true;
     });
+})
 
 
-    //node.on("mouseover", focus).on("mouseout", unfocus);
-});
-simulation.tick(10);
+
+// // Load and Draw Data
+// link_data = d3.csv('/links')
+// jsonfile = d3.json("/jsondata").then(function (data) {
+
+//     console.log(data)
+
+//     xScale = d3.scaleLinear()
+//         .domain(d3.extent(data.nodes, (d) => d.x))
+//         .range([0, width])
+
+//     yScale = d3.scaleLinear()
+//         .domain(d3.extent(data.nodes, (d) => d.y))
+//         .range([0, height])
+
+//     simulation.nodes(data.nodes);
+//     simulation.force("link").links(data.links);
+
+//     node = node
+//         .data(data.nodes)
+//         .enter().append("circle")
+//         .attr("cx", function (d) { return xScale(d.x); })
+//         .attr("cy", function (d) { return yScale(d.y); })
+//         .attr("class", "node")
+//         .attr("r", 6)
+//         .style("fill", "rgb(172, 220, 114)")
+//         .style("opacity", 0.8)
+//         .on('mouseover', nodeOverFunction)
+//         .on('mousemove', () => tooltip.style("top", (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px"))
+//         .on('mouseout', nodedOutFunction);
+//     // .call(drag(simulation));
+
+//     link_data = data.links
+//     link = link
+//         .data(data.links)
+//         .enter()
+//         .append("line")
+//         .attr("stroke", "#aaa")
+//         .attr("stroke-width", "1px")
+//         .attr("x1", (d) => xScale(d.source.x))
+//         .attr("y1", (d) => yScale(d.source.y))
+//         .attr("x2", (d) => xScale(d.target.x))
+//         .attr("y2", (d) => yScale(d.target.y))
+
+
+//     data.links.forEach(function (d) {
+//         adjlist[d.source.index + "-" + d.target.index] = true;
+//         adjlist[d.target.index + "-" + d.source.index] = true;
+//     });
+
+//     svg.call(zoom.transform, transform);
+// })
 
 function ticked() {
-
+    simulation.stop();
     // link.attr("x1", function (d) { return d.source.x; })
     //     .attr("y1", function (d) { return d.source.y; })
     //     .attr("x2", function (d) { return d.target.x; })
     //     .attr("y2", function (d) { return d.target.y; });
-        
+
 
     // node.attr("cx", function (d) { return d.x; })
     //     .attr("cy", function (d) { return d.y; });
@@ -132,14 +196,13 @@ function nodeOverFunction(d) {
         .style('opacity', 1)
         .transition()
         .duration(100)
-        .attr('r', '12px')
-
+        .attr('r', '8px')
 
     var index = d3.select(d3.event.target).datum().index;
-    node.style("opacity", function (o) {
+    nodes.style("opacity", function (o) {
         return neigh(index, o.index) ? 1 : 0.1;
     });
-    link.style("opacity", function (o) {
+    links.style("opacity", function (o) {
         return o.source.index == index || o.target.index == index ? 1 : 0.1;
     });
 
@@ -150,11 +213,11 @@ function nodedOutFunction() {
         .transition()
         .duration(100)
         .style('opacity', 0.8)
-        .attr('r', '8px')
+        .attr('r', '4px')
     tooltip.style("visibility", "hidden")
 
-    node.style("opacity", 0.8);
-    link.style("opacity", 0.8);
+    nodes.style("opacity", 0.8);
+    links.style("opacity", 0.8);
 }
 
 tooltip = d3.select("body").append("div")
@@ -162,6 +225,12 @@ tooltip = d3.select("body").append("div")
     .style("position", "absolute")
     .style("visibility", "hidden")
     .text("");
+
+
+function neigh(a, b) {
+    return a == b || adjlist[a + "-" + b];
+}
+
 
 drag = simulation => {
 
@@ -188,6 +257,14 @@ drag = simulation => {
         .on("end", dragended);
 }
 
-function neigh(a, b) {
-    return a == b || adjlist[a + "-" + b];
+
+function updateLinks() {
+    console.log("Updating links")
+};
+
+
+function updateNodes(data) {
+    console.log('Updating Nodes')
+    nodes = d3.selectAll('.nodes')
+    nodes.data(data, (d) => d.sub)
 }
